76ed710e7bfaa3f3af3f071d7802b80929990609
Pranavpaudel <pranav.paudel.123@gmail.com>
2025-02-21 20:04:11 +0545

    Loopn added for right weight type

--- demo.py
+++ demo.py
@@ -3,13 +3,16 @@ weight = float(input("Your weight: "))
 
 weight_type= input("(k)gs or (l)bs: ")
 weight_type.lower()
+while weight_type != "k" and weight_type!="l":
+    print("Invalid input. Type k for kg and l for lbs")
+    weight_type = input("(k)gs or (l)bs: ")
 
 if weight_type == "k":
     weight = weight * 2.2
     print("your weight in lbs is " + str(weight)+ "lbs")
 
 elif weight_type == "l":
-    weight = weight/ 2.2
+    weight = weight / 2.2
     print("Your weight in kgs is "+ str(weight) +  "kgs")
 
-else: print("Invalid input. Type k for kg and l for lbs")
+
