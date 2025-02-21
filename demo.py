6688fa5e2faaae686d3a910277f1f5bb75537728
Pranavpaudel <pranav.paudel.123@gmail.com>
2025-02-21 11:24:33 +0545

    Add 1st  demo file

---
+++ demo.py
@@ -0,0 +1,15 @@
+
+weight = float(input("Your weight: "))
+
+weight_type= input("(k)gs or (l)bs: ")
+weight_type.lower()
+
+if weight_type == "k":
+    weight = weight * 2.2
+    print("your weight in lbs is " + str(weight)+ "lbs")
+
+elif weight_type == "l":
+    weight = weight/ 2.2
+    print("Your weight in kgs is "+ str(weight) +  "kgs")
+
+else: print("Invalid input. Type k for kg and l for lbs")
